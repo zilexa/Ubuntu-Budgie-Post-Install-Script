4e6e0c46011158a49955847a1193f22b142df825
ZileXa <zilexa@gmx.com>
2020-04-17 17:33:45 +0200

    Add files via upload

---
+++ Brotherprinter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Brother MFC-L2750DW 32-bit drivers on 64-bit Ubuntu 19.10 or higher.
+# Brother only provides 32-bit drivers and has removed the required instructions for 64-bit OS. 
+
+sudo ufw allow 54925/udp
+sudo ufw allow 54921/tcp
+sudo apt --yes install lib32z1 libncurses5:i386 lib32stdc++6 gcc-4.8-base:i386 libgcc1:i386 libc6:i386
+echo "deb http://nl.archive.ubuntu.com/ubuntu bionic-updates main" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install lib32ncurses5
+wget --no-check-certificate https://download.brother.com/welcome/dlf103530/mfcl2750dwpdrv-4.0.0-1.i386.deb
+dpkg  -i mfcl2750dwpdrv-4.0.0-1a.i386.deb
+rm mfcl2750dwpdrv-4.0.0-1a.i386.deb
+
+# /usr/share/ppd/Brother/
+#ia32-libs or lib32stdc++ is required to be installed.
