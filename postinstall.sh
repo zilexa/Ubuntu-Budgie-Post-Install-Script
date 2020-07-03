07400e2c45ab4f71d507fc1b5eb82cf26a203a71
ZileXa <zilexa@gmx.com>
2020-07-03 17:42:14 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -36,21 +36,13 @@ gsettings set org.nemo.preferences show-reload-icon-toolbar true
 # get brightness, volume etc buttons on laptop keyboard to work
 gsettings set org.onboard layout '/usr/share/onboard/layouts/Full Keyboard.onboard'
 
-#_________________________________
-# Get separators for Plank or Dock
-# --------------------------------
-# Get a seperator-like app icon
-sudo wget --no-check-certificate -P /usr/share/icons https://github.com/zilexa/UB-PostInstall/blob/master/separators/separatorH.svg
-# Get a seperator-like app shortcut
-wget --no-check-certificate -P $HOME/.local/share/applications https://raw.githubusercontent.com/zilexa/UB-PostInstall/master/separators/SeparatorH1.desktop
-#
-
 #________________________________
 # AppImageLauncher - integrate AppImage apps on first execution
 # -------------------------------
 sudo add-apt-repository ppa:appimagelauncher-team/stable
 sudo apt update
 sudo apt install appimagelauncher
+
 
 #________________________________
 # MS Office fonts
@@ -60,45 +52,12 @@ sudo bash officefonts.sh
 wait
 rm officefonts.sh
 exit 0
-
 #________________________________
 # LibreOffice Dutch UI/Spellcheck/Hyphencheck/Help
 # -------------------------------
 sudo apt-add-repository ppa:libreoffice/ppa
 sudo apt update
 sudo apt-get install libreoffice-l10n-nl hunspell-nl hyphen-nl libreoffice-help-nl
-
-#________________________________
-# Timeshift - automated system snapshots (backups) 
-# -------------------------------
-sudo add-apt-repository -y ppa:teejee2008/timeshift
-sudo apt update
-sudo apt-get install timeshift
-
-#________________________________
-# Pluma - better simple notepad 
-# -------------------------------
-sudo apt-get install pluma
-
-#________________________________
-# VLC - better videoplayer
-# -------------------------------
-sudo apt-get install vlc
-
-#______________________________________
-# Get a Firefox shortcut for 2 profiles
-# -------------------------------------
-wget --no-check-certificate -P $HOME/.local/share/applications https://raw.githubusercontent.com/zilexa/UB-PostInstall/master/firefox.desktop
-
-#________________________________
-# Recommended apps to install manually
-# -------------------------------
-echo -e "\n\nPlease install the following recommended apps by downloading them manually:\n"
-echo -e "BLEACHBIT (cleanup) \t https://www.bleachbit.org/download/linux"
-echo -e "NOMACHINE (share desktop within local network) \t https://www.nomachine.com/download/download&id=4"
-echo -e "ANYDESK (remote desktop via internet) \t https://anydesk.com/en/downloads/linux"
-echo -e "DIGIKAM (photo management) \t https://www.digikam.org/download/"
-echo -e "RAWTHERAPEE ART (raw photo editor) \t https://bitbucket.org/agriggio/art/downloads/"
 
 
 #_________________________________
@@ -109,15 +68,52 @@ sudo apt-get install lm-sensors
 sudo add-apt-repository ppa:linrunner/tlp -y
 sudo apt install tlp
 sudo apt install hardinfo
+
 #_________________________________
 # Install applets required for Panel
 # --------------------------------
 sudo apt install budgie-kangaroo-applet
 sudo apt install budgie-workspace-wallpaper-applet
 sudo apt install budgie-calendar-applet
-
-
+#_________________________________
+# Get separators for Plank or Dock
+# --------------------------------
+# Get a seperator-like app icon
+sudo wget --no-check-certificate -P /usr/share/icons https://github.com/zilexa/UB-PostInstall/blob/master/separators/separatorH.svg
+# Get a seperator-like app shortcut
+wget --no-check-certificate -P $HOME/.local/share/applications https://raw.githubusercontent.com/zilexa/UB-PostInstall/master/separators/SeparatorH1.desktop
 #_________________________________
 # Apply panel layout
 # --------------------------------
 sudo wget --no-check-certificate -P /usr/share/budgie-desktop/layouts https://raw.githubusercontent.com/zilexa/myconfig-ubuntu/master/ubuntubudgie.layout
+
+
+#______________________________________
+# Get a Firefox shortcut for 2 profiles
+# -------------------------------------
+wget --no-check-certificate -P $HOME/.local/share/applications https://raw.githubusercontent.com/zilexa/UB-PostInstall/master/firefox.desktop
+
+
+#________________________________
+# Timeshift - automated system snapshots (backups) 
+# -------------------------------
+sudo add-apt-repository -y ppa:teejee2008/timeshift
+sudo apt update
+sudo apt-get install timeshift
+#________________________________
+# Pluma - better simple notepad 
+# -------------------------------
+sudo apt-get install pluma
+#________________________________
+# VLC - better videoplayer
+# -------------------------------
+sudo apt-get install vlc
+#________________________________
+# Recommended apps to install manually
+# -------------------------------
+echo -e "\n\nPlease install the following recommended apps by downloading them manually:\n"
+echo -e "BLEACHBIT (cleanup) \t https://www.bleachbit.org/download/linux"
+echo -e "NOMACHINE (share desktop within local network) \t https://www.nomachine.com/download/download&id=4"
+echo -e "ANYDESK (remote desktop via internet) \t https://anydesk.com/en/downloads/linux"
+echo -e "DIGIKAM (photo management) \t https://www.digikam.org/download/"
+echo -e "RAWTHERAPEE ART (raw photo editor) \t https://bitbucket.org/agriggio/art/downloads/"
