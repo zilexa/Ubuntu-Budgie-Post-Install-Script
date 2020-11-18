2c107ecd24982e850fdbd09085b0c3979a06b0bd
ZileXa <zilexa@gmx.com>
2020-11-18 23:34:08 +0100

    Update laptop-server-shares.sh

--- custom/laptop-server-shares.sh
+++ custom/laptop-server-shares.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
 
-# Mount the NFS share of my server at boot
-sudo mkdir /mnt/Obelix
+# Mount the NFS share of my server 
 sudo mount -t nfs -o nfsvers=4,minorversion=2,proto=tcp,fsc,nocto 192.168.88.2: /mnt/Obelix
+
+# Mount the share also at boot (if the above fails, check if NFS-Server has been installed like this: 
+# https://github.com/zilexa/Homeserver/blob/master/NFSv4.2-How-To.md
+sudo mkdir /mnt/Obelix
 echo "#192.168.88.2:  /mnt/Obelix  nfs4  nfsvers=4,minorversion=2,proto=tcp,fsc,nocto  0  0" | sudo tee -a /etc/fstab
 
 # Add links to the NFS share in the respective personal folders 
