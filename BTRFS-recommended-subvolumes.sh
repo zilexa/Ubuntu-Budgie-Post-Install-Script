2f43ed69585aa4b2356329f2cf3f0cb1e6aae30e
ZileXa <zilexa@gmx.com>
2020-12-28 02:17:55 +0100

    Update BTRFS-recommended-subvolumes.sh

--- BTRFS-recommended-subvolumes.sh
+++ BTRFS-recommended-subvolumes.sh
@@ -39,21 +39,30 @@ rm -r stold
 # Create subvolume for personal userdata
 sudo mount -o subvolid=5 /dev/nvme0n1p2 /mnt
 sudo btrfs subvolume create /mnt/userdata
+sudo umount /mnt
+
+# Now mount the subvolume, note this will not persist after reboot
+sudo mount -o subvol=@userdata /dev/nvme0n1p2 /mnt/userdata
 
 # Move personal user folders to the subvolume
 # Note I have already moved Desktop and Templates to my Documents folder via my config.sh file.  
-sudo mv /home/$LOGNAME/Documents /mnt/@userdata/
-sudo mv /home/$LOGNAME/Downloads /mnt/@userdata/
-sudo mv /home/$LOGNAME/Media /mnt/@userdata/
-sudo mv /home/$LOGNAME/Music /mnt/@userdata/
-sudo mv /home/$LOGNAME/Photos /mnt/@userdata/
+sudo mv /home/$LOGNAME/Documents /mnt/userdata/
+sudo mv /home/$LOGNAME/Downloads /mnt/userdata/
+sudo mv /home/$LOGNAME/Media /mnt/userdata/
+sudo mv /home/$LOGNAME/Music /mnt/userdata/
+sudo mv /home/$LOGNAME/Photos /mnt/userdata/
 
 # Link personal folders inside subvolume back into home subvolume
-ln -s /mnt/@userdata/Documents /mnt/@home/asterix/Documents
-ln -s /mnt/@userdata/Downloads /mnt/@home/asterix/Downloads
-ln -s /mnt/@userdata/Media /mnt/@home/asterix/Media
-ln -s /mnt/@userdata/Music /mnt/@home/asterix/Music
-ln -s /mnt/@userdata/Photos /mnt/@home/asterix/Photos
+ln -s /mnt/userdata/Documents /mnt/home/asterix/Documents
+ln -s /mnt/userdata/Downloads /mnt/home/asterix/Downloads
+ln -s /mnt/userdata/Media /mnt/home/asterix/Media
+ln -s /mnt/userdata/Music /mnt/home/asterix/Music
+ln -s /mnt/userdata/Photos /mnt/home/asterix/Photos
 
-# Unmount the BTRFS root
-sudo umount /mnt
+# Add a commented line in /etc/fstab, user will need to add the UUID
+# This makes the /mnt/userdata mount persistent. 
+echo "# Mount the BTRFS root subvolume @userdata" | sudo tee -a /etc/fstab
+echo "UUID=!!COPY-PASTE-FROM-ABOVE /mnt/userdata           btrfs   defaults,noatime,subvol=@userdata 0       2" | sudo tee -a /etc/fstab
+
+# Now open fstab for the user to copy paste the UUID
+sudo nano /etc/fstab
