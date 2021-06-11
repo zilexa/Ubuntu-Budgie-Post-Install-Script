bf8c682f9a6f367793f48f8ebdf824bff75cf04d
ZileXa <zilexa@gmx.com>
2021-06-11 09:14:14 +0200

    Update defaultlanguage.sh

--- onlyoffice/defaultlanguage.sh
+++ onlyoffice/defaultlanguage.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-cd $HOME/Downloads
-wget -O new.docx https://raw.githubusercontent.com/zilexa/Ubuntu-Budgie-Post-Install-Script/master/onlyoffice/new.docx
-wget -O new.pptx https://raw.githubusercontent.com/zilexa/Ubuntu-Budgie-Post-Install-Script/master/onlyoffice/new.pptx
-wget -O new.xlsx https://raw.githubusercontent.com/zilexa/Ubuntu-Budgie-Post-Install-Script/master/onlyoffice/new.xlsx
+cd /opt/onlyoffice/desktopeditors/converter/empty/
+sudo mkdir nl-NL
+sudo wget -O nl-NL/new.docx https://raw.githubusercontent.com/zilexa/Ubuntu-Budgie-Post-Install-Script/master/onlyoffice/new.docx
+sudo wget -O nl-NL/new.pptx https://raw.githubusercontent.com/zilexa/Ubuntu-Budgie-Post-Install-Script/master/onlyoffice/new.pptx
+sudo wget -O nl-NL/new.xlsx https://raw.githubusercontent.com/zilexa/Ubuntu-Budgie-Post-Install-Script/master/onlyoffice/new.xlsx
 
-echo "============================================================================" 
-echo "                                                                            " 
-echo "The default document, spreadsheet and presentation have been downloaded.    " 
-echo "These are in NL language, if you want to keep this language hit y and Enter " 
-echo "                                                                            " 
-echo "============================================================================" 
+echo "==========================================================================================" 
+echo "                                                                                          " 
+echo "OnlyOffice default document language & spellcheck is US English.                          "
+echo "To change it to your language, you need to adjust the default template.                   "
+echo "OnlyOffice has no way of doing this normally and permissions prevent you to do this easily" 
+echo "                                                                                          "
+echo "To use Dutch (NL) hit 'y' and 'ENTER.'                                                    " 
+echo "                                                                                          "
+echo "To use a different language, hit "n" and "ENTER" and follow the instructions.             "
+echo "                                                                                          " 
+echo "==========================================================================================" 
 case ${answer:0:1} in
     y|Y )
-    sudo mkdir /opt/onlyoffice/converters/nl-NL
-    sudo cp new.* /opt/onlyoffice/converters/nl-NL/
-    cd /opt/onlyoffice/converters/nl-NL/
+    
+    sudo cp nl-NL/new.*
+    
     sudo chmod 644 new.docx new.pptx new.xlsx
     sudo chown root:root new.docx new.pptx new.xlsx
     sudo rm /opt/onlyoffice/converters/
