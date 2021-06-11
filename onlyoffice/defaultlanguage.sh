2b602501a6125adbccd568e430bf0e2a7e1cf916
ZileXa <zilexa@gmx.com>
2021-06-11 10:45:36 +0200

    Update defaultlanguage.sh

--- onlyoffice/defaultlanguage.sh
+++ onlyoffice/defaultlanguage.sh
@@ -19,17 +19,20 @@ echo "                                                                          
 echo "==========================================================================================" 
 case ${answer:0:1} in
     y|Y )
-    
-    sudo cp nl-NL/new.*
-    
-    sudo chmod 644 new.docx new.pptx new.xlsx
-    sudo chown root:root new.docx new.pptx new.xlsx
-    sudo rm /opt/onlyoffice/converters/
-    
-    TO FINISH LATER 
-    
+    echo "Replace the template files to support nl-NL default document language.." 
+    # Copy the templates to the converter/empty dir
+    sudo cp nl-NL/new.* ./
+    # Remove the templates with prefix in_ and _mm as we need to replace them
+    sudo rm in_*
+    sudo rm mm_*
+    # Copy the new.* templates and rename the copies by adding in_ prefix
+    for f in new.*; do sudo cp -- "$f" "in_$f"; done
+    # Rename the new.* templates, add mm_ 
+    for f in new.*; do sudo mv -- "$f" "mm_$f"; done
+    echo "done!" 
     ;;
     * )
-        echo "Not installing all win10/office365 fonts..."
+        sudo xdg-open nl-NL/
+        echo "Follow the instructions in that link". 
     ;;
 esac
